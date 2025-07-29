31817cbddd7baf30121deeff8b38aa7d54172afb
Wanjiku <wanjikumwaura0304@gmail.com>
2025-07-29 06:37:06 +0300

    complete lab

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,7 +6,7 @@ import {
   screen,
   waitForElementToBeRemoved,
 } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
 import { server } from "../mocks/server";
 
 import App from "../components/App";
